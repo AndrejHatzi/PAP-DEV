80e9b84a023b0433b9f3a82f76730f2c1ada5860
AndrejHatzi <45495944+AndrejHatzi@users.noreply.github.com>
2019-01-07 12:13:46 +0000

    Add files via upload

--- body.py
+++ body.py
@@ -1,8 +1,11 @@
-from head import function_types, keywords, assign
-from functions import var_evaluation, partial_print_evaluation
+from head import function_types, keywords, assign, errors, data_types
+from functions import var_evaluation, partial_print_evaluation, deep_var_evaluation, hybrid_var_evaluation
+import re
 KontrolFlow = []
 Kvars = {
     "init" : 'TRUE',
+    "var2" : 5,
+    "randomtext" : ['Kittens r cool', data_types["string"]]
 }
 def iteration(file):
     with open(file) as f:
@@ -11,13 +14,25 @@ def iteration(file):
             if (cObject[1] == keywords["variable"]):
                 EqualSignIndex = line.index("=") + 2
                 toEval = line[EqualSignIndex:]
-                type,valor = var_evaluation(toEval)
+                #print(toEval)
+                #type,valor = var_evaluation(toEval)
                 if (cObject[0] in Kvars):
                     Kvars[cObject[0]] = [valor, type]
                     KontrolFlow.append([assign["change_value"], cObject[0], valor])
                 else:
-                    Kvars[cObject[0]] = [valor, type]
-                    KontrolFlow.append([assign["assign_var"], cObject[0], valor])
+                    toEval = line[EqualSignIndex-1:]
+                    #Kvars[cObject[0]] = [valor, type]
+                    #KontrolFlow.append([assign["assign_var"], cObject[0], valor])
+                    value = hybrid_var_evaluation(Kvars, toEval)
+                    '''
+                    if (value == errors['error2']):
+                        pass
+                    else:
+                        Kvars[cObject[0]] = value
+                        KontrolFlow.append([assign["assign_var"], cObject[0], value])
+                    '''
+
+
 
             if (cObject[0] == keywords["print"]):
                 gate = line.index("(") + 1
@@ -30,6 +45,6 @@ def iteration(file):
                 KontrolFlow.append([assign["print_call"], keywords["print"], valor])
 
 
-iteration('script.txt')
+iteration('script2.txt')
 print(KontrolFlow)
 print(Kvars)
